6542a23d80e58127aa0cbf6544ea63953a1c7378
Nathan Araujo <99274283+nathan-araujo@users.noreply.github.com>
2022-04-25 23:40:33 +0000

    added consts to js

--- js/script.js
+++ js/script.js
@@ -15,11 +15,15 @@ function cardio () {
 
 	//declare constants
 	const RUNNING30 = 1.91558442
-	const
+	const RUNNING60 = 3.83116883
+	const HIKING30 = 1.2012987
+	const HIKING60 = 2.4025974
+	const SWIMMING30 = 1.65584416
+	const SWIMMING60 = 3.31168831
 	
 	//process
-	let benchpress = (weight * 2)
+	let output = (weight * RUNNING30)
 	
   // output
-  document.getElementById('60mins').innerHTML = 'You should bench press around: ' + benchpress.toFixed(2) + 'lbs'
+  document.getElementById('60mins').innerHTML = 'You should bench press around: ' + output.toFixed(2) + 'lbs'
 }
